d0b0dc2df21ca8f7f0deeb493a414adef76087bb
Vesperino <61016469+Vesperino@users.noreply.github.com>
2025-11-06 15:37:47 +0100

    fix

---
+++ backend/PortalForge.Infrastructure/Migrations/20251106143704_FixDepartmentSubstituteRelationships2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalForge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDepartmentSubstituteRelationships2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SpecificDepartmentRoleType",
+                schema: "public",
+                table: "RequestApprovalStepTemplates",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpecificDepartmentRoleType",
+                schema: "public",
+                table: "RequestApprovalStepTemplates");
+        }
+    }
+}
